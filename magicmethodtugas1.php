2b4a017964e4289c1379933968d91a9c474255f3
bayyubi <ubayrafif.js2@gmail.com>
2026-07-14 11:21:15 +0700

    PHP

--- magicmethodtugas1.php
+++ magicmethodtugas1.php
@@ -1,6 +1,6 @@
 <?php
 
-class Produk {
+class Book {
     public $buku;
     public $kode;
     public $status;
@@ -12,10 +12,19 @@ class Produk {
     }
 
     public function __toString() {
-        return "Buku: " . $this->buku . ", kode: " . $this->kode . ", status: " . $this->status;
+        return "Buku: " . $this->buku . "</br> " . "kode: " . $this->kode . "</br> " . "status: " . $this->status;
     }
 }
+    
+$buku1 = new Book("Kitab Hacker", "6769", "Tersedia");
+$buku2 = new Book("Prompt Engineering", "9696", "Tersedia");
+$buku3 = new Book("Belajar PHP-in", "1234", "Dipinjam");
 
-$perpustakaan = new Produk("Kitab Hacker", "6769", "Tersedia");
+echo "<h1>Perpustakaan</h1>";
 
-echo $perpustakaan;
+echo "<h3>Informasi Buku</h3>";
+echo $buku1;
+echo "<br><br>";
+echo $buku2;
+echo "<br><br>";
+echo $buku3;
