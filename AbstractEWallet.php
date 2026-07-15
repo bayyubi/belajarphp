65c2288708678f9980f1d96d69e73eb56f213c70
bayyubi <ubayrafif.js2@gmail.com>
2026-07-15 08:06:18 +0700

    PHP

---
+++ AbstractEWallet.php
@@ -0,0 +1,17 @@
+<?php
+
+class EWallet extends Pembayaran {
+    private $nomorHp;
+    
+    public function __construct($total, $nHp) {
+        parent::__constrcuct($total);
+        $this->nomorHp = $noHp;
+    }
+
+    public function prosesTransaksi() {
+        echo "Menghubungkan ke e-wallet dengan nomor HP: " . $this->nomorHp . "<br>";
+        echo "Status: Saldo e-wallet berhasil dipotong sebesar Rp " . number_format($this->totalBayar, 0, ',', '.') . "<br>";
+    }
+}
+
+//Lanjut AbstractEksekusi.php
