6bc69c85f319155ee762f9b49d1365e3f37cbcb5
bayyubi <ubayrafif.js2@gmail.com>
2026-07-15 08:41:26 +0700

    PHP

--- AbstractEksekusi.php
+++ AbstractEksekusi.php
@@ -4,12 +4,14 @@ require_once 'AbstractPembayaran.php';
 require_once 'AbstractTransferBank.php';
 require_once 'AbstractEWallet.php';
 
-echo "<h3>Transaksi 1:</h3>";
+echo "<center><h1>Riwayat Transaksi</h1></center>";
+
+echo "<hr><h3>Transaksi 1:</h3>";
 $transaksiBank = new TransferBank(800000, "676-767-6767");
 $transaksiBank->tampilkanNota();
 $transaksiBank->prosesTransaksi();
 
 echo "<hr><h3>Transaksi 2:</h3>";
-$transaksiOvo = new EWallet(500000, "0812-3456-7890");
+$transaksiOvo = new EWallet(500000, "0867-6767-6767");
 $transaksiOvo->tampilkanNota();
 $transaksiOvo->prosesTransaksi();
