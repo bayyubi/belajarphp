49250390cb2587eb5c2a7d4a53a2564c01366116
bayyubi <ubayrafif.js2@gmail.com>
2026-07-27 08:37:07 +0700

    Tugas 8

---
+++ Cookie/cookie.php
@@ -0,0 +1,28 @@
+<?php
+
+$user = 'bayu';
+$password = '123';
+
+if (isset($_POST['submit']) ){
+
+    if( $_POST['nama'] == $user &&
+        $_POST['password'] == $password){
+
+        setcookie('nama', $_POST['nama'], time()+120);
+
+
+        header('Location: profile.php?nama=' . $user);
+
+
+    }else{
+        echo 'login gagal!';
+    }
+}
+
+?> 
+
+<form action="cookie.php" method="post">
+    <input type="text" name="nama">
+    <input type="password" name="password">
+    <input type="submit" name="submit">
+</form>
