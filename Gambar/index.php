81608985e05e21c886ff30a73bed79d2a66c0048
bayyubi <ubayrafif.js2@gmail.com>
2026-08-03 22:34:04 +0700

    Tugas 10

---
+++ Gambar/index.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    print_r($_FILES);
+
+    $nama = $_FILES['gambar']['name'];
+    $asal = $_FILES['gambar']['tmp_name'];
+
+    move_uploaded_file($asal, 'upload/'. $nama);
+
+}
+
+?>
+
+<form action="index.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="gambar">
+    <input type="submit" name="submit" value="upload">
+</form>
