aa088ca045c5bd71259c5ec15cf66dc6dba158b6
bayyubi <ubayrafif.js2@gmail.com>
2026-08-04 14:24:44 +0700

    tugas 12

--- Gambar/index.php
+++ Gambar/index.php
@@ -1,17 +1,25 @@
 <?php
 
-if (isset($_POST['submit'])) {
+if (isset($_POST['submit'])){
     print_r($_FILES);
 
     $nama = $_FILES['gambar']['name'];
     $asal = $_FILES['gambar']['tmp_name'];
+    $tujuan = 'upload/' . $nama;
 
-    move_uploaded_file($asal, 'upload/'. $nama);
+    if (move_uploaded_file($asal, $tujuan)){
+        echo "<h2> upload berhasil </h2>";
+        echo "<p> nama file : <b> $nama </b> </p>";
+        echo "<img src='$tujuan' width='300'>";
+    }
+    else{
+        echo "gagal upload";
+    }
 }
 
 ?>
 
-<form action="index.php" method="post" enctype="multipart/form-data">
+<form action="foto.php" method="post" enctype="multipart/form-data">
     <input type="file" name="gambar">
     <input type="submit" name="submit" value="upload">
 </form>
